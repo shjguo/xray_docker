6250960e6739290e214f0be278f09499edf5d679
shjguo <sh1989622@gmail.com>
2023-09-24 13:54:23 +0800

    config file directory

--- reality/entrypoint.sh
+++ reality/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -f /config_info.txt ]; then
+if [ -f /xray/config_info.txt ]; then
   echo "config.json exist"
 else
   IPV6=$(curl -6 -sSL --connect-timeout 3 --retry 2  ip.sb || echo "null")
@@ -17,7 +17,7 @@ else
 
   if [ -n "$HOSTMODE_PORT" ];then
     EXTERNAL_PORT=$HOSTMODE_PORT
-    jq ".inbounds[0].port=$HOSTMODE_PORT" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+    jq ".inbounds[0].port=$HOSTMODE_PORT" /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
   fi
 
   if [ -z "$DEST" ]; then
@@ -45,48 +45,48 @@ else
   fi
 
   # change config
-  jq ".inbounds[0].settings.clients[0].id=\"$UUID\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[0].streamSettings.realitySettings.dest=\"$DEST\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[0].settings.clients[0].id=\"$UUID\"" /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
+  jq ".inbounds[0].streamSettings.realitySettings.dest=\"$DEST\"" /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
 
   SERVERNAMES_JSON_ARRAY="$(echo "[$(echo $SERVERNAMES | awk '{for(i=1;i<=NF;i++) printf "\"%s\",", $i}' | sed 's/,$//')]")"
-  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[0].streamSettings.realitySettings.serverNames = $serverNames' /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq --argjson serverNames "$SERVERNAMES_JSON_ARRAY" '.inbounds[0].streamSettings.realitySettings.serverNames = $serverNames' /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
 
-  jq ".inbounds[0].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
-  jq ".inbounds[0].streamSettings.network=\"$NETWORK\"" /config.json >/config.json_tmp && mv /config.json_tmp /config.json
+  jq ".inbounds[0].streamSettings.realitySettings.privateKey=\"$PRIVATEKEY\"" /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
+  jq ".inbounds[0].streamSettings.network=\"$NETWORK\"" /xray/config.json >/xray/config.json_tmp && mv /xray/config.json_tmp /xray/config.json
 
 
 
 
   FIRST_SERVERNAME=$(echo $SERVERNAMES | awk '{print $1}')
   # config info with green color
-  echo -e "\033[32m" >/config_info.txt
-  echo "IPV6: $IPV6" >>/config_info.txt
-  echo "IPV4: $IPV4" >>/config_info.txt
-  echo "UUID: $UUID" >>/config_info.txt
-  echo "DEST: $DEST" >>/config_info.txt
-  echo "PORT: $EXTERNAL_PORT" >>/config_info.txt
-  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/config_info.txt
-  echo "PRIVATEKEY: $PRIVATEKEY" >>/config_info.txt
-  echo "PUBLICKEY: $PUBLICKEY" >>/config_info.txt
-  echo "NETWORK: $NETWORK" >>/config_info.txt
+  echo -e "\033[32m" >/xray/config_info.txt
+  echo "IPV6: $IPV6" >>/xray/config_info.txt
+  echo "IPV4: $IPV4" >>/xray/config_info.txt
+  echo "UUID: $UUID" >>/xray/config_info.txt
+  echo "DEST: $DEST" >>/xray/config_info.txt
+  echo "PORT: $EXTERNAL_PORT" >>/xray/config_info.txt
+  echo "SERVERNAMES: $SERVERNAMES (任选其一)" >>/xray/config_info.txt
+  echo "PRIVATEKEY: $PRIVATEKEY" >>/xray/config_info.txt
+  echo "PUBLICKEY: $PUBLICKEY" >>/xray/config_info.txt
+  echo "NETWORK: $NETWORK" >>/xray/config_info.txt
   if [ "$IPV4" != "null" ]; then
-    SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#wulabing_docker_vless_reality_vision"
-    echo "IPV4 订阅连接: $SUB_IPV4" >>/config_info.txt
-    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/config_info.txt
+    SUB_IPV4="vless://$UUID@$IPV4:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#docker_vless_reality_vision"
+    echo "IPV4 订阅连接: $SUB_IPV4" >>/xray/config_info.txt
+    echo -e "IPV4 订阅二维码:\n$(echo "$SUB_IPV4" | qrencode -o - -t UTF8)" >>/xray/config_info.txt
   fi
   if [ "$IPV6" != "null" ];then
-    SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#wulabing_docker_vless_reality_vision"
-    echo "IPV6 订阅连接: $SUB_IPV6" >>/config_info.txt
-    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/config_info.txt
+    SUB_IPV6="vless://$UUID@$IPV6:$EXTERNAL_PORT?encryption=none&security=reality&type=$NETWORK&sni=$FIRST_SERVERNAME&fp=chrome&pbk=$PUBLICKEY&flow=xtls-rprx-vision#docker_vless_reality_vision"
+    echo "IPV6 订阅连接: $SUB_IPV6" >>/xray/config_info.txt
+    echo -e "IPV6 订阅二维码:\n$(echo "$SUB_IPV6" | qrencode -o - -t UTF8)" >>/xray/config_info.txt
   fi
 
 
-  echo -e "\033[0m" >>/config_info.txt
+  echo -e "\033[0m" >>/xray/config_info.txt
 
 fi
 
 # show config info
-cat /config_info.txt
+cat /xray/config_info.txt
 
 # run xray
-exec /xray -config /config.json
+exec /xray -config /xray/config.json
